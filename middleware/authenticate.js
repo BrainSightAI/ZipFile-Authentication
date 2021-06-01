bf56f1a76392787befd2c433ff46c62fd06c6003
unknown <dipakdip33@.com>
2021-06-01 23:20:25 +0530

    optimised

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,10 +1,9 @@
+const express = require('express')
 const fs = require('fs')
 const admzip = require('adm-zip')
 const path = require('path')
-const { hostname } = require('os')
 const decompress = require("decompress")
-const { brotliDecompress } = require('zlib')
-
+const app = express()
 
 function authenticate(req, res, next) {
     var dirPath = process.cwd() + "/public/compressed/" + req.files[0].originalname;
