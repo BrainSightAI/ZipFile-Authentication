b578ba0c441d086894090ccb9cb727ad5169be17
unknown <dipakdip33@.com>
2021-06-02 19:53:04 +0530

    azure file connection sucessfully

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,23 +1,44 @@
-const express = require('express')
-const fs = require('fs')
-const admzip = require('adm-zip')
-const path = require('path')
-const decompress = require("decompress")
-const app = express()
+const express = require("express");
+const fs = require("fs");
+const admzip = require("adm-zip");
+const path = require("path");
+const decompress = require("decompress");
+const app = express();
+
+// const MulterAzureStorage = require('multer-azure-blob-storage').MulterAzureStorage;
+
+const {
+    ShareServiceClient,
+    StorageSharedKeyCredential,
+} = require("@azure/storage-file-share");
+// Enter your storage account name and shared key
+const account = "zipfilestorages";
+const accountKey =
+    "etf5UNKhZK/XBoXxvo4tGAKE3hRoqAuABQaOawjOvEYrhnyl/rVZ9tTEkpDMfAl1OrunymaPC5iz/XujMU1lig==";
+
+// Use StorageSharedKeyCredential with storage account and account key
+// StorageSharedKeyCredential is only available in Node.js runtime, not in browsers
+const credential = new StorageSharedKeyCredential(account, accountKey);
+const serviceClient = new ShareServiceClient(
+    // When using AnonymousCredential, following url should include a valid SAS
+    `https://${account}.file.core.windows.net`,
+    credential
+);
 
 function authenticate(req, res, next) {
-    var dirPath = process.cwd() + "/public/compressed/" + req.files[0].originalname;
+    var dirPath =
+        process.cwd() + "/public/compressed/" + req.files[0].originalname;
     var destPath = process.cwd() + "/public/unzip";
 
     //unzipping the file
     (async() => {
         try {
             const files = await decompress(dirPath, destPath);
-            console.log("unziping is done")
+            console.log("unziping is done");
         } catch (error) {
             console.log(error);
         }
-        readFile(destPath)
+        readFile(destPath);
     })();
 
     //authorization is process
@@ -31,21 +52,50 @@ function authenticate(req, res, next) {
                     var outputFilePath = "output.zip";
                     if (files) {
                         files.forEach((file) => {
-                            zip.addLocalFile(destPath + "/" + file)
+                            zip.addLocalFile(destPath + "/" + file);
                         });
                         fs.writeFileSync(outputFilePath, zip.toBuffer());
+                        shareName = "zip-file";
+                        directoryName = "zipfiledata";
+                        //save the file in azure file
+                        async function main() {
+                            const directoryClient = serviceClient
+                                .getShareClient(shareName)
+                                .getDirectoryClient(directoryName);
+
+                            const content = "Hello World!hhhh";
+                            const fileName = outputFilePath;
+                            const fileClient = directoryClient.getFileClient(fileName);
+                            await fileClient.create(content.length);
+                            console.log(`Create file ${fileName} successfully`);
+
+                            // Upload file range
+                            await fileClient.uploadRange(content, 0, content.length);
+                            console.log(
+                                `Upload file range "${content}" to ${fileName} successfully`
+                            );
+
+                            let shareIter = serviceClient.listShares();
+                            let i = 1;
+                            for await (const share of shareIter) {
+                                console.log(`Share${i}: ${share.name}`);
+                                i++;
+                            }
+                        }
+
+                        main();
                     }
-                    console.log("authorization provided")
+                    console.log("authorization provided");
                     break;
                 }
-                //authorization is faled
+                //authorization is failed
                 if (ext != ".json") {
-                    return res.status(401).send({ message: "No authorization provided" })
+                    return res.status(401).send({ message: "No authorization provided" });
                     break;
                 }
             }
-        })
+        });
         next();
     }
 }
-module.exports = authenticate
+module.exports = authenticate;
--- route/router.js
+++ route/router.js
@@ -3,34 +3,6 @@ const router = express.Router();
 const fs = require('fs')
 const multer = require('multer')
 const authenticate = require('../middleware/authenticate')
-    // const MulterAzureStorage = require('multer-azure-blob-storage').MulterAzureStorage;
-
-const { ShareServiceClient, StorageSharedKeyCredential } = require("@azure/storage-file-share");
-// Enter your storage account name and shared key
-const account = "zipfilestorages";
-const accountKey = "etf5UNKhZK/XBoXxvo4tGAKE3hRoqAuABQaOawjOvEYrhnyl/rVZ9tTEkpDMfAl1OrunymaPC5iz/XujMU1lig==";
-
-// Use StorageSharedKeyCredential with storage account and account key
-// StorageSharedKeyCredential is only available in Node.js runtime, not in browsers
-const credential = new StorageSharedKeyCredential(account, accountKey);
-const serviceClient = new ShareServiceClient(
-    // When using AnonymousCredential, following url should include a valid SAS
-    `https://${account}.file.core.windows.net`,
-    credential
-);
-
-async function main() {
-    let shareIter = serviceClient.listShares();
-    let i = 1;
-    for await (const share of shareIter) {
-        console.log(`Share${i}: ${share.name}`);
-        i++;
-    }
-}
-
-main();
-
-
 var dir = "public";
 var subDirectory = "public/compressed";
 var subDirectory = "public/compressed";
@@ -56,31 +28,6 @@ var storage = multer.diskStorage({
 });
 var compressfilesupload = multer({ storage: storage });
 
-
-// const yourCustomLogic = (req, file) => {
-//     return Date.now().toString() + "." + file.originalname.toLowerCase().split(" ").join("-")
-// }
-
-// const resolveBlobName = (req, file) => {
-//     return new Promise((resolve, reject) => {
-//         const blobName = yourCustomLogic(req, file);
-//         resolve(blobName);
-//     });
-// };
-
-// const azureStorage = new MulterAzureStorage({
-//     connectionString: 'DefaultEndpointsProtocol=https;AccountName=vuestorage1;AccountKey=w8hTY+1EWs9ox+NhL2JrM9cl6P129UqVNKmfaRubgveU06VlaIVODdhn1157QyO2nDoeM6stfDcG9UuBGWhGaQ==;EndpointSuffix=core.windows.net',
-//     accessKey: 'w8hTY+1EWs9ox+NhL2JrM9cl6P129UqVNKmfaRubgveU06VlaIVODdhn1157QyO2nDoeM6stfDcG9UuBGWhGaQ==',
-//     accountName: 'vuestorage1',
-//     containerName: 'vue-js-mri',
-//     blobName: resolveBlobName,
-//     containerAccessLevel: 'blob',
-//     urlExpirationTime: 60
-// });
-
-// const upload = multer({
-//     storage: azureStorage
-// });
 
 router.get("/", (req, res) => {
     res.sendFile(process.cwd() + "/index.html")
