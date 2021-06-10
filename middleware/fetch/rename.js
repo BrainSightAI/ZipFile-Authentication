e4ad65193b01de48044c5ad1b9cea0700e058d4e
unknown <dipakdip33@.com>
2021-06-10 12:24:52 +0530

    file processing

--- middleware/fetch/rename.js
+++ middleware/fetch/rename.js
@@ -6,20 +6,6 @@ class renamefile {
         const newPath = (process.cwd() + "/diacomfolder/" + count++)
         fs.renameSync(currentPath, newPath)
         console.log("Successfully moved the file!")
-            // this.removefolder()
     }
-
-    // async removefolder() {
-    //     const dir = process.cwd() + "/zipyfy/unzip";
-
-    //     // delete directory recursively
-    //     try {
-    //         await fs.rmdirSync(dir, { recursive: true });
-
-    //         console.log(`${dir} is deleted!`);
-    //     } catch (err) {
-    //         console.error(`Error while deleting ${dir}.`);
-    //     }
-    // }
 }
 module.exports = renamefile;
